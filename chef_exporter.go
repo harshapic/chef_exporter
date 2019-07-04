2f171ad6ecf3a2db7c5265c184dcaf0ca172bb63
halvi sriharsha <harsha.pic@gmail.com>
2019-07-04 00:51:44 -0700

    updated

--- chef_exporter.go
+++ chef_exporter.go
@@ -24,6 +24,17 @@ type Exporter struct {
 	ManageRedisStatus           prometheus.Gauge
 	ManageWebStatus             prometheus.Gauge
 	ManageWorkerStatus          prometheus.Gauge
+        ServerBookshelfStatus           prometheus.Gauge
+        ServerEcsyncStatus             prometheus.Gauge
+        ServerNginxStatus          prometheus.Gauge
+        ServerOcbifrostStatus           prometheus.Gauge
+        ServerOcidStatus             prometheus.Gauge
+        ServerErchefStatus          prometheus.Gauge
+        ServerExpanderStatus           prometheus.Gauge
+        ServerSolr4Status             prometheus.Gauge
+        ServerPostgresrStatus          prometheus.Gauge
+        ServerRabbitmqStatus           prometheus.Gauge
+        ServerRedisStatus             prometheus.Gauge
 }
 
 func NewExporter() *Exporter {
@@ -43,6 +54,62 @@ func NewExporter() *Exporter {
 			Name:      "ManageWorkerStatus",
 			Help:      "ManageWorkerStatus",
 		}),
+                ServerBookshelfStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerBookshelfStatus",
+                        Help:      "ServerBookshelfStatus",
+                }),
+                ServerEcsyncStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerEcsyncStatus",
+                        Help:      "ServerEcsyncStatus",
+                }),
+                ServerNginxStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerNginxStatus",
+                        Help:      "ServerNginxStatus",
+                }),
+                ServerOcbifrostStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerOcbifrostStatus",
+                        Help:      "ServerOcbifrostStatus",
+                }),
+                ServerOcidStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerOcidStatus",
+                        Help:      "ServerOcidStatus",
+                }),
+                ServerErchefStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerErchefStatus",
+                        Help:      "ServerErchefStatus",
+                }),
+                ServerExpanderStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerExpanderStatus",
+                        Help:      "ServerExpanderStatus",
+                }),
+                ServerSolr4Status: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerSolr4Status",
+                        Help:      "ServerSolr4Status",
+                }),
+                ServerPostgresrStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerPostgresrStatus",
+                        Help:      "ServerPostgresrStatus",
+                }),
+                ServerRabbitmqStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerRabbitmqStatus",
+                        Help:      "ServerRabbitmqStatus",
+                }),
+                ServerRedisStatus: prometheus.NewGauge(prometheus.GaugeOpts{
+                        Namespace: namespace,
+                        Name:      "ServerRedisStatus",
+                        Help:      "ServerRedisStatus",
+                }),
+
 	}
 }
 
@@ -51,28 +118,117 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	e.ManageRedisStatus.Describe(ch)
 	e.ManageWebStatus.Describe(ch)
 	e.ManageWorkerStatus.Describe(ch)
+        e.ServerBookshelfStatus.Describe(ch)
+        e.ServerEcsyncStatus.Describe(ch)
+        e.ServerNginxStatus.Describe(ch)
+        e.ServerOcbifrostStatus.Describe(ch)
+        e.ServerOcidStatus.Describe(ch)
+        e.ServerErchefStatus.Describe(ch)
+        e.ServerRabbitmqStatus.Describe(ch)
+        e.ServerExpanderStatus.Describe(ch)
+        e.ServerSolr4Status.Describe(ch)
+        e.ServerPostgresrStatus.Describe(ch)
+        e.ServerRedisStatus.Describe(ch)
 }
 
 // Collect implements the prometheus.Collector interface.
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	if execute("chef-manage-ctl status | grep redis | awk {'print $1'}") == "run:" {
+        manage := strings.Split(execute("chef-manage-ctl status"), "\n")
+        server := strings.Split(execute("chef-server-ctl status"), "\n")
+        m := make(map[string]string)
+        for _,s := range manage {
+          t := strings.Split(s," ")
+          m[t[1]]=t[0]
+        }
+        for _,s := range server {
+          t := strings.Split(s," ")
+          m[t[1]]=t[0]
+        }
+	if m["redis:"] == "run:" {
 	   e.ManageRedisStatus.Set(1)
 	} else {
 	   e.ManageRedisStatus.Set(0)
 	}
-        if execute("chef-manage-ctl status | grep web | awk {'print $1'}") == "run:" {
+        if m["web:"] == "run:" {
            e.ManageWebStatus.Set(1)
         } else {
            e.ManageWebStatus.Set(0)
         }
-        if execute("chef-manage-ctl status | grep worker | awk {'print $1'}") == "run:" {
+        if m["worker:"] == "run:" {
            e.ManageWorkerStatus.Set(1)
         } else {
            e.ManageWorkerStatus.Set(0)
         }
-	e.ManageRedisStatus.Collect(ch)
+        if m["bookshelf:"] == "run:" {
+           e.ServerBookshelfStatus.Set(1)
+        } else {
+           e.ServerBookshelfStatus.Set(0)
+        }
+        if m["ec_sync_client:"] == "run:" {
+           e.ServerEcsyncStatus.Set(1)
+        } else {
+           e.ServerEcsyncStatus.Set(0)
+        }
+        if m["nginx:"] == "run:" {
+           e.ServerNginxStatus.Set(1)
+        } else {
+           e.ServerNginxStatus.Set(0)
+        }
+	if m["oc_bifrost:"] == "run:" {
+           e.ServerOcbifrostStatus.Set(1)
+        } else {
+           e.ServerOcbifrostStatus.Set(0)
+        }
+        if m["oc_id:"] == "run:" {
+           e.ServerOcidStatus.Set(1)
+        } else {
+           e.ServerOcidStatus.Set(0)
+        }
+        if m["opscode-erchef:"] == "run:" {
+           e.ServerErchefStatus.Set(1)
+        } else {
+           e.ServerErchefStatus.Set(0)
+        }
+	if m["opscode-expander:"] == "run:" {
+           e.ServerExpanderStatus.Set(1)
+        } else {
+           e.ServerExpanderStatus.Set(0)
+        }
+        if m["opscode-solr4:"] == "run:" {
+           e.ServerSolr4Status.Set(1)
+        } else {
+           e.ServerSolr4Status.Set(0)
+        }
+        if m["postgresql:"] == "run:" {
+           e.ServerPostgresrStatus.Set(1)
+        } else {
+           e.ServerPostgresrStatus.Set(0)
+        }
+	if m["rabbitmq:"] == "run:" {
+           e.ServerRabbitmqStatus.Set(1)
+        } else {
+           e.ServerRabbitmqStatus.Set(0)
+        }
+        if m["redis_lb:"] == "run:" {
+           e.ServerRedisStatus.Set(1)
+        } else {
+           e.ServerRedisStatus.Set(0)
+        }
+	
+        e.ManageRedisStatus.Collect(ch)
 	e.ManageWebStatus.Collect(ch)
 	e.ManageWorkerStatus.Collect(ch)
+        e.ServerBookshelfStatus.Collect(ch)
+        e.ServerEcsyncStatus.Collect(ch)
+        e.ServerNginxStatus.Collect(ch)
+        e.ServerOcbifrostStatus.Collect(ch)
+        e.ServerOcidStatus.Collect(ch)
+        e.ServerErchefStatus.Collect(ch)
+        e.ServerRabbitmqStatus.Collect(ch)
+        e.ServerExpanderStatus.Collect(ch)
+        e.ServerSolr4Status.Collect(ch)
+        e.ServerPostgresrStatus.Collect(ch)
+        e.ServerRedisStatus.Collect(ch)
 }
 
 func execute(cmd string)(s string) {
